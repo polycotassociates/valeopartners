be3629dd07ca1d0179fe4153e724d9086563439b
Aaron Engler <aaron@polycotassociates.com>
2019-01-18 10:14:40 -0500

    fix for extra = sign

--- web/modules/custom/vp_views/src/Plugin/views/area/ExportHeader.php
+++ web/modules/custom/vp_views/src/Plugin/views/area/ExportHeader.php
@@ -40,8 +40,8 @@ class ExportHeader extends AreaPluginBase {
     // Get the current path
     $path = \Drupal::request()->getpathInfo();
     //Put them together with /export added to the path and format=xls at the end
-    $export_xls = "$path/export?$query=&_format=xls";
-    $export_csv = "$path/export?$query=&_format=csv";
+    $export_xls = "$path/export?$query&_format=xls";
+    $export_csv = "$path/export?$query&_format=csv";
     // html for link
     // $export_xls_link = "<span id='export-xls-link'><span class='xls-icon'>&nbsp;</span><a href='$export_xls'><img src='/themes/custom/valeo_classic/images/xls-24.png' />Export XLS</a></span>";
     $export_csv_link = "<span id='export-csv-link'><span class='csv-icon'>&nbsp;</span><a href='$export_csv'><img src='/themes/custom/valeo_classic/images/csv-24.png' />Export CSV</a></span>";
