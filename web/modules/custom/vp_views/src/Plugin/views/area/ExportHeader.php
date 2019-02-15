dc7cf1eae0a99ff0a2b60e599798e3973cdaaa9e
Aaron Engler <aaron@polycotassociates.com>
2019-02-15 11:30:13 -0500

    Cleaning up export header, removing csv export in favor of only xls export

--- web/modules/custom/vp_views/src/Plugin/views/area/ExportHeader.php
+++ web/modules/custom/vp_views/src/Plugin/views/area/ExportHeader.php
@@ -22,33 +22,28 @@ class ExportHeader extends AreaPluginBase {
    */
   public function render($empty = FALSE) {
     if (!$empty || !empty($this->options['empty'])) {
-      return array(
-        '#markup' => $this->make_links(),
-      );
+      return ['#markup' => $this->makeLinks()];
     }
-
-    return array();
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  private function makeLinks() {
 
-  private function make_links(){
-
-    // Get the query from the url
+    // Get the query from the url.
     $q = $_GET;
-    // Build the query from the query variables
+    // Build the query from the query variables.
     $query = http_build_query($q);
-    // Get the current path
+    // Get the current path.
     $path = \Drupal::request()->getpathInfo();
-    //Put them together with /export added to the path and format=xls at the end
+    // Put them together with /export added to the path and format=xls at the end.
     $export_xls = "$path/export?$query&_format=xls";
-    $export_csv = "$path/export?$query&_format=csv";
-    // html for link
-    // $export_xls_link = "<span id='export-xls-link'><span class='xls-icon'>&nbsp;</span><a href='$export_xls'><img src='/themes/custom/valeo_classic/images/xls-24.png' />Export XLS</a></span>";
-    $export_csv_link = "<span id='export-csv-link'><span class='csv-icon'>&nbsp;</span><a href='$export_csv'><img src='/themes/custom/valeo_classic/images/csv-24.png' />Export CSV</a></span>";
+    // $export_csv = "$path/export?$query&_format=csv";
+    $export_xls_link = "<span id='export-xls-link'><span class='xls-icon'>&nbsp;</span><a href='$export_xls'><img src='/themes/custom/valeo_classic/images/xls-24.png' />Export XLS</a></span>";
+    //$export_csv_link = "<span id='export-csv-link'><span class='csv-icon'>&nbsp;</span><a href='$export_csv'><img src='/themes/custom/valeo_classic/images/csv-24.png' />Export CSV</a></span>";
 
-    // return link
-    return "$export_csv_link $export_xls_link";
-
-    
+    // Return the link.
+    return " $export_xls_link";
   }
 }
