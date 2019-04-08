7471fc15e0e04fd495f724b50e7131bb36d90fcc
Aaron Engler <aaron@polycotassociates.com>
2019-04-08 10:07:13 -0400

    Views plugins for low/average/high rates, standard, actual

---
+++ web/modules/custom/vp_views/src/Plugin/views/field/HighActualRate.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\vp_views\Plugin\views\field;
+
+/**
+ * @file
+ * Definition of Drupal\vp_views\Plugin\views\field\HighActualRate.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\node\Entity\Node;
+
+/**
+ * Gets the highest rate given a position/rate/firm.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("high_actual_rate")
+ */
+class HighActualRate extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+
+    $bundle = $values->_entity->bundle();
+
+    switch ($bundle) {
+      case "vp_type_rate":
+
+        // Get the relationships from the value.
+        $relationships = $values->_relationship_entities;
+        // Get the position id.
+        $position = $values->_entity->field_vp_rate_position->getValue()[0]['target_id'];
+        // Get the firm id.
+        $firm = $values->_entity->field_vp_rate_firm->getValue()[0]['target_id'];
+        // Get the related filing.
+        $filing = $relationships['field_vp_rate_filing'];
+        // Get the end year from the rate.
+        $rate_year = $filing->get('field_vp_filing_year_end')->getValue()[0]['value'];
+        // Get the results from the method.
+        $results = $this->getHighActualRate($rate_year, $position, $firm);
+
+        break;
+
+    }
+
+    return $results;
+  }
+
+  /**
+   * Get the low actual rate from a rate year, position and firm.
+   */
+  private function getHighActualRate($rate_year, $position, $firm) {
+
+    $db = \Drupal::database();
+
+    // Get low actual rate by year/position/firm. Lowest is first result sorted by ASC.
+
+    $query = $db->select('node', 'node')->fields('node');
+    $query->join('node__field_vp_rate_hourly', 'hourly', 'node.nid = hourly.entity_id');
+    $query->join('node__field_vp_rate_filing', 'filing', 'node.nid = filing.entity_id');
+    $query->join('node__field_vp_rate_position', 'position', 'node.nid = position.entity_id');
+    $query->join('node__field_vp_rate_firm', 'firm', 'node.nid = firm.entity_id');
+    $query->join('node__field_vp_filing_year_end', 'year', 'year.entity_id = filing.field_vp_rate_filing_target_id');
+    $query->fields('node', ['nid']);
+    $query->fields('hourly', ['field_vp_rate_hourly_value']);
+    $query->fields('year', ['field_vp_filing_year_end_value']);
+    $query->fields('position', ['field_vp_rate_position_target_id']);
+    $query->fields('firm', ['field_vp_rate_firm_target_id']);
+    $group = $query->andConditionGroup()
+      ->condition('field_vp_filing_year_end_value', $rate_year, '=')
+      ->condition('field_vp_rate_position_target_id', $position, '=')
+      ->condition('field_vp_rate_firm_target_id', $firm, '=');
+    $query->orderBy('field_vp_rate_hourly_value', 'DESC');
+
+    $result = $query->condition($group)->execute()->fetchAll();
+
+    if ($result) {
+      $rate = $result[0]->field_vp_rate_hourly_value;
+      return $rate;
+    }
+  }
+
+}
--- web/modules/custom/vp_views/src/Plugin/views/field/RFPPosition.php
+++ web/modules/custom/vp_views/src/Plugin/views/field/RFPPosition.php
@@ -1,24 +1,23 @@
 <?php
 
-namespace Drupal\vp_views\Plugin\views\field;
-
 /**
  * @file
- * Definition of Drupal\vp_views\Plugin\views\field\StandardRateByYear.
+ * Definition of Drupal\vp_views\Plugin\views\field\RFPPosition.
  */
 
+namespace Drupal\vp_views\Plugin\views\field;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 
 /**
- * Field handler show a rate value by given year.
+ * Position field for a rate for RFP.
  *
  * @ingroup views_field_handlers
  *
- * @ViewsField("standard_rate_by_year")
+ * @ViewsField("rfp_position")
  */
-class StandardRateByYear extends FieldPluginBase {
+class RFPPosition extends FieldPluginBase {
 
   /**
    * @{inheritdoc}
@@ -33,8 +32,6 @@ class StandardRateByYear extends FieldPluginBase {
    */
   protected function defineOptions() {
     $options = parent::defineOptions();
-    $year = format_date(time(), 'custom', 'Y');
-    $options['year'] = ['default' => $year];
     return $options;
   }
 
@@ -42,17 +39,6 @@ class StandardRateByYear extends FieldPluginBase {
    * Provide the options form.
    */
   public function buildOptionsForm(&$form, FormStateInterface $form_state) {
-    // Get current year as 4 digit date.
-
-    $form['year'] = [
-      '#type' => 'textfield',
-      '#required' => TRUE,
-      '#title' => $this->t('Year'),
-      '#description' => $this->t('The 4 digit year for this rate.'),
-      '#default_value' => $this->options['year'],
-
-    ];
-
     parent::buildOptionsForm($form, $form_state);
   }
 
@@ -64,23 +50,6 @@ class StandardRateByYear extends FieldPluginBase {
     $bundle = $values->_entity->bundle();
 
     switch ($bundle) {
-
-      case "vp_type_individual":
-
-        // Relationships on the individual node.
-        $relationships = $values->_relationship_entities;
-        $firm = $relationships['field_vp_employment_history'];
-        // Get the individual id.
-        $individual_nid = $values->_entity->get('nid')->getValue()[0]['value'];
-        // Get the firm id.
-        // $firm_nid = $firm->get('field_firm')->getValue()[0]['target_id'];
-        // Get the year from the form.
-        $year = $this->options['year'];
-        // Call getRateByYear method.
-        $results = $this->getRateByYear($year, $individual_nid);
-
-        break;
-
       case "vp_type_rate":
 
         // Relationships on the individual node.
@@ -103,30 +72,30 @@ class StandardRateByYear extends FieldPluginBase {
   }
 
   /**
-   * Get the highest standard rate from an attorney for a particular year.
+   * Get the highest hourly rate from an attorney for a particular year.
    */
   private function getRateByYear($year, $individual_nid) {
 
     $db = \Drupal::database();
 
     $query = $db->select('node', 'node')->fields('node');
-    $query->join('node__field_vp_rate_standard', 'standard', 'node.nid = standard.entity_id');
+    $query->join('node__field_vp_rate_hourly', 'hourly', 'node.nid = hourly.entity_id');
     $query->join('node__field_vp_rate_filing', 'filing', 'node.nid = filing.entity_id');
     $query->join('node__field_vp_rate_individual', 'individual', 'node.nid = individual.entity_id');
     $query->join('node__field_vp_filing_year_end', 'year', 'year.entity_id = filing.field_vp_rate_filing_target_id');
     $query->fields('node', ['nid']);
-    $query->fields('standard', ['field_vp_rate_standard_value']);
+    $query->fields('hourly', ['field_vp_rate_hourly_value']);
     $query->fields('year', ['field_vp_filing_year_end_value']);
     $query->fields('individual', ['field_vp_rate_individual_target_id']);
     $group = $query->andConditionGroup()
       ->condition('field_vp_filing_year_end_value', $year, '=')
       ->condition('field_vp_rate_individual_target_id', $individual_nid, '=');
-    $query->orderBy('field_vp_rate_standard_value', 'DESC');
+    $query->orderBy('field_vp_rate_hourly_value', 'DESC');
     $query->range(0, 1);
     $result = $query->condition($group)->execute()->fetchAll();
 
     if ($result) {
-      $rate = $result[0]->field_vp_rate_standard_value;
+      $rate = $result[0]->field_vp_rate_hourly_value;
       return $rate;
     }
   }
