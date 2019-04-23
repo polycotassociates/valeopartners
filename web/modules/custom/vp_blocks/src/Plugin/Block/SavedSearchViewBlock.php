b39e1344db88cf8c7a9667db6ae5e4752fd36f7a
Aaron Engler <aaron@polycotassociates.com>
2019-04-23 14:21:40 -0400

    Create saved search views, view logic

---
+++ web/modules/custom/vp_blocks/src/Plugin/Block/SavedSearchViewBlock.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\vp_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+use Drupal\views\Views;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Render\Renderer;
+
+/**
+ * Provides a block that renders a view contextually.
+ *
+ * @Block(
+ *   id = "saved_search_view_block",
+ *   admin_label = @Translation("Saved Search Block"),
+ * )
+ */
+class SavedSearchViewBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    // 0 = detailed results, 1 = summary results.
+
+    $search_type = $node->get('field_vp_search_type')->getValue()[0]['value'];
+
+    switch ($search_type) {
+
+      case 0:
+        $view = Views::getView('saved_search_detail');
+        return [
+          '#type' => 'view',
+          '#name' => 'saved_search_detail',
+          '#view' => $view,
+          '#display_id' => 'saved_search_detail_block',
+          '#embed' => TRUE,
+        ];
+
+      case 1:
+        $view = Views::getView('saved_search_summary');
+        return [
+          '#type' => 'view',
+          '#name' => 'saved_search_summary',
+          '#view' => $view,
+          '#display_id' => 'saved_search_summary_page',
+          '#embed' => TRUE,
+        ];
+    }
+
+  }
+
+}
--- web/modules/custom/vp_views/src/Plugin/views/area/ExportHeader.php
+++ web/modules/custom/vp_views/src/Plugin/views/area/ExportHeader.php
@@ -3,6 +3,7 @@
 namespace Drupal\vp_views\Plugin\views\area;
 
 use Drupal\views\Plugin\views\area\AreaPluginBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Defines a views area plugin.
@@ -19,10 +20,10 @@ class ExportHeader extends AreaPluginBase {
   public function render($empty = FALSE) {
     if (!$empty || !empty($this->options['empty'])) {
 
-      $node = \Drupal::request()->attributes->get('node');
+      $node = \Drupal::routeMatch()->getParameter('node');
 
       if ($node && $node->bundle() == ('vp_type_saved_search')) {
-        return ['#markup' => $this->makeLinksForSearch(1)];
+        return ['#markup' => $this->makeLinksForSearch($node->id())];
       }
       else {
         return ['#markup' => $this->makeLinks()];
@@ -51,64 +52,197 @@ class ExportHeader extends AreaPluginBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Create a link for the Saved Search content type.
    */
   private function makeLinksForSearch($nid) {
 
-    // Get the query from the url.
-    $q = $_GET;
-    // Build the query from the query variables.
-    $query = '?bar_year[0]=2012&bar_year[1]=2013&location[0]=580';
-    // $query1 = $this->generateQueryFromNode();
-    // Get the current path.
-    $path = \Drupal::request()->getpathInfo();
+    // Build the query from the saved search items for the node $nid.
+    $query = $this->generateQueryFromNode($nid);
+
+    // $query = "firm=12857&";
     // Put them together with /export added to the path and format=xls at the end.
-    $export_xls = "/saved/search/export?$query&_format=xls";
+    $export_xls = "/saved/search/export?$query";
+    $export_xls .= "_format=xls";
     // Create the html for the link.
-    $export_xls_link = "$query<span id='export-xls-link'><span class='xls-icon'>&nbsp;</span><a href='$export_xls'><img src='/themes/custom/valeo_classic/images/xls-24.png' />Export Results as XLS</a></span>";
+    $export_xls_link = "<span id='export-xls-link'><span class='xls-icon'>&nbsp;</span><a href='$export_xls'><img src='/themes/custom/valeo_classic/images/xls-24.png' />Export Results as XLS</a></span>";
     // Return the link.
 
-    // $node = Drupal::request()->attributes->get('node');
+    print_r($export_xls);
 
-    // $firms = FALSE;
-    // if ($node->get('field_vp_search_firms')) {
-    //   $search_firms = $node->get('field_vp_search_firms')->getValue();
-    //   foreach ($search_firms as $firm) {
-    //     $firms[] = $firm['target_id'];
-    //   }
-    // }
-    // $firm_query = '';
-    // for ($x = 0; $x <= count($firms); $x++) {
-    //   $firm_query .= "firm[$x]=" . $firms[$x] . '&';
-    // }
-
-    print_r($nid);
     return "$export_xls_link";
 
   }
 
   /**
-   * {@inheritdoc}
+   * Create the query string from the saved search node.
    */
-  private function generateQueryFromNode() {
+  private function generateQueryFromNode($nid) {
 
-    $node = Drupal::request()->attributes->get('node');
+    $query = '';
+    $node = Node::load($nid);
 
-    $firms = FALSE;
+
+
+
+    $firms = [];
     if ($node->get('field_vp_search_firms')) {
       $search_firms = $node->get('field_vp_search_firms')->getValue();
       foreach ($search_firms as $firm) {
         $firms[] = $firm['target_id'];
       }
     }
-    $firm_query = '';
-    for ($x = 0; $x <= count($firms); $x++) {
-      $firm_query .= "firm[$x]=" . $firms[$x] . '&';
+
+    $positions = [];
+    if ($node->get('field_vp_search_position')) {
+      $search_positions = $node->get('field_vp_search_position')->getValue();
+      foreach ($search_positions as $position) {
+        $positions[] = $position['target_id'];
+      }
     }
 
-    return $firm_query;
+    $industries = [];
+    if ($node->get('field_vp_search_industry')) {
+      $search_industries = $node->get('field_vp_search_industry')->getValue();
+      foreach ($search_industries as $industry) {
+        $industries[] = $industry['target_id'];
+      }
+    }
+
+    $cities = [];
+    if ($node->get('field_vp_search_region_city')) {
+      $search_cities = $node->get('field_vp_search_region_city')->getValue();
+      foreach ($search_cities as $city) {
+        $cities[] = $city['target_id'];
+      }
+    }
+
+    $practice_areas = [];
+    if ($node->get('field_vp_search_practice_area')) {
+      $search_practice_areas = $node->get('field_vp_search_practice_area')->getValue();
+      foreach ($search_practice_areas as $practice_area) {
+        $practice_areas[] = $practice_area['target_id'];
+      }
+    }
+
+    $grad_date_from = FALSE;
+    $grad_range = [];
+    if ($grad_date_from = $node->get('field_vp_search_grad_date')->getValue()) {
+      $grad_date_from = $node->get('field_vp_search_grad_date')->getValue()[0]['from'];
+      $grad_date_to = $node->get('field_vp_search_grad_date')->getValue()[0]['to'];
+      if (!$grad_date_to) {
+        $grad_date_to = $grad_date_from;
+      }
+      $grad_range = range($grad_date_from, $grad_date_to);
+    }
+
+    $partner_date_from = FALSE;
+    $partner_range = [];
+    if ($node->get('field_vp_search_partner_year')->getValue()) {
+      $partner_date_from = $node->get('field_vp_search_partner_year')->getValue()[0]['from'];
+      $partner_date_to = $node->get('field_vp_search_partner_year')->getValue()[0]['to'];
+      if (!$partner_date_to) {
+        $partner_date_to = $partner_date_from;
+      }
+      $partner_range = range($partner_date_from, $partner_date_to);
+    }
+
+    $bar_date_from = FALSE;
+    if ($node->get('field_vp_search_bar_year')->getValue()) {
+      $bar_date_from = $node->get('field_vp_search_bar_year')->getValue()[0]['from'];
+      $bar_date_to = $node->get('field_vp_search_bar_year')->getValue()[0]['to'];
+      if (!$bar_date_to) {
+        $bar_date_to = $bar_date_from;
+      }
+      $bar_range = range($bar_date_from, $bar_date_to);
+    }
+
+    $rate_date_from = FALSE;
+    if ($node->get('field_vp_search_rate_year')->getValue()) {
+      $rate_date_from = $node->get('field_vp_search_rate_year')->getValue()[0]['from'];
+      $rate_date_to = $node->get('field_vp_search_rate_year')->getValue()[0]['to'];
+      if (!$rate_date_to) {
+        $rate_date_to = $rate_date_from;
+      }
+      $rate_range = range($rate_date_from, $rate_date_to);
+    }
+
+    // Create firms query string.
+    // if (count($firms) > 1) {
+    //   for ($x = 0; $x < count($firms); $x++) {
+    //     $query .= "firm[$x]=" . $firms[$x] . '&';
+    //   }
+    // }
+    // else {
+    //   $query .= "firm=" . $firms[0] . '&';
+    // }
+
+    // if (count($positions) > 1) {
+    //   for ($x = 0; $x < count($positions); $x++) {
+    //     $query .= "position[$x]=" . $positions[$x] . '&';
+    //   }
+    // }
+    // else {
+    //   $query .= "position=" . $positions[0] . '&';
+    // }
+
+    // if (count($industries) > 1) {
+    //   for ($x = 0; $x < count($industries); $x++) {
+    //     $query .= "industry[$x]=" . $industries[$x] . '&';
+    //   }
+    // }
+    // else {
+    //   $query .= "industry=" . $industries[0] . '&';
+    // }
+
+    // if (count($cities) > 1) {
+    //   for ($x = 0; $x < count($cities); $x++) {
+    //     $query .= "location[$x]=" . $cities[$x] . '&';
+    //   }
+    // }
+    // else {
+    //   $query .= "location=" . $cities[0] . '&';
+    // }
+
+    // if (count($practice_areas) > 1) {
+    //   for ($x = 0; $x < count($practice_areas); $x++) {
+    //     $query .= "practice_area[$x]=" . $practice_areas[$x] . '&';
+    //   }
+    // }
+    // else {
+    //   $query .= "practice_area=" . $practice_areas[0] . '&';
+    // }
+
+    $firms_query = $firms ? implode(",", $firms) : 'all';
+    $query .= "firm=$firms_query&";
+
+    $positions_query = $positions ? implode(",", $positions) : 'all';
+    $query .= "position=$positions_query&";
+
+    $industries_query = $industries ? implode(",", $industries) : 'all';
+    $query .= "industry=$industries_query&";
+
+    $cities_query = $cities ? implode(",", $cities) : 'all';
+    $query .= "location=$cities_query&";
+
+    // $grad_date_query = $grad_date_from ? implode(",", $grad_range) : 'all';
+    // $partner_date_query = $partner_date_from ? implode(",", $partner_range) : 'all';
+    // $bar_date_query = $bar_date_from ? implode(",", $bar_range) : 'all';
+    // $rate_date_query = $rate_date_from ? implode(",", $rate_range) : 'all';
+
+    // $query .= "graduation_year=$grad_date_query&";
+    // $query .= "partner=$partner_date_query&";
+    // $query .= "bar_year=$bar_date_query&";
+    // $query .= "rate=$rate_date_query&";
+
+    // https://valeopartners.lndo.site/saved/search/export?firm[0]=12857&firm[1]=12858&position=2124&industry=2613&location=580&practice_area=4017&graduation_year=all&partner=all&bar_year=all&_format=xls
+
+    // https://valeopartners.lndo.site/saved/search/export?firm=12857,12858&position=2111&industry=2613&location=580&practice_area=4017&_format=xls
+
+
+    //https://valeopartners.lndo.site/saved/search/export?firm=12857,12858&position=2111&industry=2613&location=580&&graduation_year=2011+2012+2013&_format=xls
+    // field_vp_rate_firm_target_id_verf[0]=257600&field_vp_graduation_value[min]=&field_vp_graduation_value[max]=&field_vp_bar_date_value[min]=&field_vp_bar_date_value[max]=&field_vp_filing_fee_dates_value[min]=&field_vp_filing_fee_dates_value[max]=&_format=xls
+    return $query;
 
   }
 
 }
-
