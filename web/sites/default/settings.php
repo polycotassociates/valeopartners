9160f4bff489d839a259db568062cab0d381a16c
Aaron Engler <aaron@polycotassociates.com>
2019-06-10 10:31:24 -0400

    remove live site redirect

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -762,7 +762,7 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
   // Redirect to https://$primary_domain in the Live environment
   if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
     /** Replace www.example.com with your registered domain name */
-    $primary_domain = 'reports.valeopartners.com';
+    //$primary_domain = 'reports.valeopartners.com';
   }
   else {
     // Redirect to HTTPS on every Pantheon environment.
