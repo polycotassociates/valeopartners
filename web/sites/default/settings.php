a38aee1d62847b3b862e08b378473ba6cc55e7fc
Aaron Engler <aaron@polycotassociates.com>
2019-08-23 13:53:27 -0400

    Remove Redis

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -753,26 +753,6 @@ if (file_exists(__DIR__ . '/settings.pantheon.php')) {
   include __DIR__ . "/settings.pantheon.php";
 }
 
-// Configure Redis
-
-if (defined('PANTHEON_ENVIRONMENT')) {
-  // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
-  $settings['container_yamls'][] = 'modules/redis/example.services.yml';
-
-  //phpredis is built into the Pantheon application container.
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  // These are dynamic variables handled by Pantheon.
-  $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
-  $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
-  $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
-
-  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
-  $settings['cache_prefix']['default'] = 'pantheon-redis';
-
-  // Set Redis to not get the cache_form (no performance difference).
-  $settings['cache']['bins']['form']      = 'cache.backend.database';
-}
-
 /**
  * Force SSL on all accounts
  */
